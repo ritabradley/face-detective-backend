25e0c507b6312bd2fda5a28b08a39be7f65b3e13
Rita Bradley <rita@ritabradley.dev>
2023-03-25 16:45:07 -0700

    :construction: build route structure for `signin` and `register`

--- server.js
+++ server.js
@@ -3,4 +3,66 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+const database = {
+	users: [
+		{
+			userId: '239t2f',
+			name: 'Rita',
+			email: 'rita@ritabradley.dev',
+			password: '8np05J^&fUf-',
+			entries: 5,
+			joined: new Date()
+		},
+		{
+			userId: '392fn1',
+			name: 'Ron',
+			email: 'ronaldhopkins904@gmail.com',
+			password: '9KnR&cT93*L',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send('wooo! we in here!')
+})
+
+// /signin --> POST == success/fail
+app.post('/signin', (req, res) => {
+	const foundUser = database.users.find(user => req.body.email === user.email && req.body.password === user.password);
+
+	if (foundUser) {
+		res.json('success');
+	} else {
+		res.status(404).json('user not found');
+	}
+})
+
+// /register --> POST == user
+app.post('/register', (req, res, ) => {
+	const {name, email, password} = req.body
+	database.users.push({
+		userId: '8h26fw',
+			name,
+			email,
+			password,
+			entries: 0,
+			joined: new Date()
+	})
+	res.json(database.users[database.users.length - 1])
+})
+
+// /profile/:userId --> GET res == user
+app.get('/profile/:userId', (req, res) => {
+	res.send('user profile')
+	console.log(req.params)
+})
+
+app.put('/image', (req, res) => {
+	res.send('image entries')
+})
+
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
