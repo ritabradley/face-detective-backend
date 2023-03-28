6902779e9e79f67bd7baf7a4bc49e37c2cf8ac19
Rita Bradley <rita@ritabradley.dev>
2023-03-28 15:35:28 -0700

    :card_file_box: update `/register` to send the user email and hash to the `login` table

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
-import knex from 'knex'
+import knex from "knex";
 
 const database = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    port : 5432,
-    user : 'rita',
-    password : process.env.DB_PASSWORD,
-    database : 'face-detective'
-  }
+	client: "pg",
+	connection: {
+		host: "127.0.0.1",
+		port: 5432,
+		user: "rita",
+		password: process.env.DB_PASSWORD,
+		database: "face-detective"
+	}
 });
 
 dotenv.config();
@@ -23,11 +23,6 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
-const saltRounds = 10;
-const hashPassword = async (password) => {
-	return await bcrypt.hash(password, saltRounds);
-};
-
 
 app.get("/", (req, res) => {
 	res.send(database.users);
@@ -37,6 +32,10 @@ app.get("/", (req, res) => {
 app.post("/signin", async (req, res) => {
 	const {email, password} = req.body;
 	const foundUser = database.users.find(user => email === user.email);
+
+	database("logins")
+		.where("email", email)
+		.insert();
 
 	if (foundUser) {
 		const isPasswordMatch = await bcrypt.compare(password, foundUser.password);
@@ -54,69 +53,80 @@ app.post("/signin", async (req, res) => {
 // /register --> POST == user
 app.post("/register", async (req, res,) => {
 	const {name, email, password} = req.body;
-	const userId = '';
+	const userId = "";
 
 	const saltRounds = 10;
 	const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-	database('users')
-		.returning('*')
-		.insert({
-			name,
-			email,
-			joined: new Date()
-	}).then(user => {
-		res.json(user[0])
-	}).catch(err => res.status(404).json('We cannot register you at this time. Please try again later.'))
+	await database.transaction(trx => {
+		database("logins")
+			.insert({
+				"email": email,
+				"hash": hashedPassword
+			})
+			.returning("email")
+			.then(loginEmail => {
+				database("users")
+					.returning("*")
+					.insert({
+						name,
+						email: loginEmail[0].email,
+						joined: new Date()
+					}).then(user => {
+					res.json(user[0]);
+				});
+			});
+	})
+		.catch(err => res.status(404).json("We cannot register you at this time. Please try again later."));
 });
 
 app.post("/check-email", (req, res) => {
-  const { email } = req.body;
-  database("users")
-    .select("email")
-    .where("email", email)
-    .then((result) => {
-      if (result.length > 0) {
-        res.status(400).json({ message: "Email already exists" });
-      } else {
-        res.json({ message: "Email is available" });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "Error checking email availability" });
-    });
+	const {email} = req.body;
+	database("users")
+		.select("email")
+		.where("email", email)
+		.then((result) => {
+			if (result.length > 0) {
+				res.status(400).json({message: "Email already exists"});
+			} else {
+				res.json({message: "Email is available"});
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({message: "Error checking email availability"});
+		});
 });
 
 // /profile/:userId --> GET res == user
 app.get("/profile/:userId", (req, res) => {
 	const {userId} = req.params;
 
-	database('users')
+	database("users")
 		.select("*")
 		.where("user_id", userId)
 		.then(user => {
 			if (user.length) {
-				res.json(user[0])
+				res.json(user[0]);
 			} else {
 				throw new Error;
 			}
 		}).catch(err => {
-			res.status(404).json("user not found");
-	})
+		res.status(404).json("user not found");
+	});
 });
 
 app.put("/image", (req, res) => {
 	const {userId} = req.body;
 
-	database('users')
-		.where('user_id', userId)
-		.increment('entries', 1)
-		.returning('entries')
+	database("users")
+		.where("user_id", userId)
+		.increment("entries", 1)
+		.returning("entries")
 		.then(entries => {
-			res.json(entries[0].entries)
+			res.json(entries[0].entries);
 		}).catch(err => {
-			res.status(400).json("unable to update entries");
-	})
+		res.status(400).json("unable to update entries");
+	});
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
