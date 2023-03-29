06d77cde29286c349e0eaa5991907eaf62ae17fb
Rita Bradley <rita@ritabradley.dev>
2023-03-28 23:56:55 -0700

    :bug: update `/signin` and `/register` to make sure userId is passed properly

--- server.js
+++ server.js
@@ -37,21 +37,31 @@ app.post("/signin", async (req, res) => {
 		.from("logins")
 		.where("email", email)
 		.then(async (response) => {
-			const isMatchedPassword = await bcrypt.compare(password, response[0].hash)
+			const isMatchedPassword = await bcrypt.compare(password, response[0].hash);
 			if (isMatchedPassword) {
-				database.select('*')
-					.from('users')
-					.where('email', email)
-					.then(user => res.json(user[0]))
-					.catch(err => res.status(400).json('unable to retrieve user'))
+				database.select("*")
+					.from("users")
+					.where("email", email)
+					.then(user => {
+						const modifiedUser = {
+							userId: user[0].user_id, // Assuming 'id' is the unique identifier in the database
+							name: user[0].name,
+							email: user[0].email,
+							entries: user[0].entries,
+							joined: user[0].joined
+						};
+						res.json(modifiedUser);
+					})
+					.catch(err => res.status(400).json("unable to retrieve user"));
 			} else {
-				res.status(400).json('Invalid email or password. Please try again.')
+				res.status(400).json("Invalid email or password. Please try again.");
 			}
 		})
 		.catch((err) => {
 			res.status(404).json("User not found");
 		});
 });
+
 
 
 // /register --> POST == user
@@ -76,14 +86,22 @@ app.post("/register", async (req, res,) => {
 						email: loginEmail[0].email,
 						joined: new Date()
 					}).then(user => {
-					res.json(user[0]);
-				});
+						const modifiedUser = {
+							userId: user[0].user_id, // Assuming 'id' is the unique identifier in the database
+							name: user[0].name,
+							email: user[0].email,
+							entries: user[0].entries,
+							joined: user[0].joined
+						};
+						res.json(modifiedUser);
+					});
 			})
 			.then(trx.commit)
 			.catch(trx.rollback);
 	})
 		.catch(err => res.status(404).json("We cannot register you at this time. Please try again later."));
 });
+
 
 app.post("/check-email", (req, res) => {
 	const {email} = req.body;
@@ -122,13 +140,13 @@ app.get("/profile/:userId", (req, res) => {
 
 app.put("/image", (req, res) => {
 	const {userId} = req.body;
-
+	console.log("Received userId:", userId);
 	database("users")
 		.where("user_id", userId)
 		.increment("entries", 1)
 		.returning("entries")
 		.then(entries => {
-			res.json(entries[0].entries);
+			res.json({entries: entries[0].entries}); // Return a JSON object
 		}).catch(err => {
 		res.status(400).json("unable to update entries");
 	});
